458579ff79d4938443933c68365b7277b1fe5b88
Shaheer Ahmad <shaheerahmad97@gmail.com>
2019-01-29 18:22:25 -0500

    Added some new feature

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -8,5 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //This was already a kotlin file for some reason so i am adding this comment to signify a change
+        // for the PR.
     }
 }
